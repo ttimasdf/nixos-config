2cf0ccde4f00a77df06f01a5f39ba35bfff02e61
ttimasdf <opensource@rabit.pw>
2025-09-02 10:42:45 +0800

    nix settings enable flakes

--- configuration.nix
+++ configuration.nix
@@ -152,6 +152,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
