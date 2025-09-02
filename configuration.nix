a4dfe66c14fd0aa5d74f47a27e601091db0ee68f
ttimasdf <opensource@rabit.pw>
2025-09-02 16:07:45 +0800

    fix fcitx5 wayland warning

--- configuration.nix
+++ configuration.nix
@@ -93,6 +93,7 @@
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
       fcitx5-chinese-addons  # table input method support
