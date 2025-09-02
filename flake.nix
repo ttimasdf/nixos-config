f506448c5d4309b4998828d8eef84f23743daaff
ttimasdf <opensource@rabit.pw>
2025-09-02 11:03:01 +0800

    add nixos-hardware lenovo-legion-16irx9h

--- flake.nix
+++ flake.nix
@@ -5,16 +5,18 @@
     # i.e. nixos-24.11
     # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs@{ self, nixpkgs, lanzaboote, ... }: {
+  outputs = inputs@{ self, nixpkgs, lanzaboote, nixos-hardware, ... }: {
     # NOTE: 'nixos' is the default hostname
     nixosConfigurations."Nokia-N97" = nixpkgs.lib.nixosSystem {
       modules = [
         lanzaboote.nixosModules.lanzaboote
+        nixos-hardware.nixosModules.lenovo-legion-16irx9h
         ./configuration.nix
       ];
     };
