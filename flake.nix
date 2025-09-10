c40b502128ca85cd1ade399ff2dd2ebc304677b3
ttimasdf <opensource@rabit.pw>
2025-09-11 07:52:50 +0800

    flake: add nixos hardware, lanzaboote, winapps

--- flake.nix
+++ flake.nix
@@ -8,8 +8,11 @@
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-unified.url = "github:srid/nixos-unified";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     # Software inputs
+    lanzaboote = { url = "github:nix-community/lanzaboote"; inputs.nixpkgs.follows = "nixpkgs"; };
+    winapps = { url = "github:winapps-org/winapps"; inputs.nixpkgs.follows = "nixpkgs"; };
     nix-index-database = { url = "github:nix-community/nix-index-database"; inputs.nixpkgs.follows = "nixpkgs"; };
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
