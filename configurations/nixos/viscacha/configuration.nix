a65684b0399732144337b669230ccad12b5f7184
ttimasdf <opensource@rabit.pw>
2025-09-12 12:34:37 +0800

    viscacha: enable hosts.corpo

--- configurations/nixos/viscacha/configuration.nix
+++ configurations/nixos/viscacha/configuration.nix
@@ -53,6 +53,8 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  modules.hosts.corpo.enable = true;
   # endregion network
 
   # region UI/UX
