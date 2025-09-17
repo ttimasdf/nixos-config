c68e846c64cdba84d50e4e9b385a701153e6dba5
ttimasdf <opensource@rabit.pw>
2025-09-17 12:54:06 +0800

    viscacha: add user group wireshark

--- configurations/nixos/viscacha/configuration.nix
+++ configurations/nixos/viscacha/configuration.nix
@@ -120,7 +120,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.u = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "wireshark" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
