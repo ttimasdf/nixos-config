67756196421704d26a9e873265ffa7be85b22d1d
ttimasdf <opensource@rabit.pw>
2025-10-11 10:13:29 +0800

    dev: change devshell name

--- modules/flake/devshell.nix
+++ modules/flake/devshell.nix
@@ -1,7 +1,7 @@
 {
   perSystem = { pkgs, ... }: {
     devShells.default = pkgs.mkShell {
-      name = "nixos-unified-template-shell";
+      name = "knownrabbit-nixos-config";
       meta.description = "Shell environment for modifying this Nix configuration";
       packages = with pkgs; [
         just
