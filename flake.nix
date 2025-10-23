c1bc7568d5de209d9a79764847fb127da263ff16
ttimasdf <opensource@rabit.pw>
2025-10-23 11:03:57 +0800

    flake: add nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Principle inputs (updated by `nix run .#update`)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nix-darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-parts.url = "github:hercules-ci/flake-parts";
