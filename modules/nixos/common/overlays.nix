736eb4876177aad762b7219868688a9e478f4a83
ttimasdf <opensource@rabit.pw>
2025-10-23 15:58:30 +0800

    pkgs: add helper to create custom packages from /packages

--- modules/nixos/common/overlays.nix
+++ modules/nixos/common/overlays.nix
@@ -1,4 +1,4 @@
-{ flake, lib, config, ... }:
+{ flake, lib, config, pkgs, ... }:
 let
   inherit (flake) self;
   overlaysPath = self + "/overlays";
@@ -19,8 +19,18 @@ let
       # Support for raw overlay files that don't need flake inputs
       (_final: _prev: imported);
 
+  overlays = (map importOverlay overlayFiles);
+
+  packagesPath = self + "/packages";
+  packages =
+    final: prev:
+    (prev.lib.packagesFromDirectoryRecursive {
+      callPackage = prev.lib.callPackageWith final;
+      directory = packagesPath;
+    });
+
 in
 {
   # Map the list of file paths to a list of overlay functions
-  nixpkgs.overlays = map importOverlay overlayFiles;
+  nixpkgs.overlays = overlays ++ [ packages ];
 }
