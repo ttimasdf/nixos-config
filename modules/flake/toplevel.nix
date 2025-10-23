2b5c5d29cb0ce972450804e9cda08aa6203fbfda
ttimasdf <opensource@rabit.pw>
2025-10-23 10:56:15 +0800

    flake: add debug flag for repl

--- modules/flake/toplevel.nix
+++ modules/flake/toplevel.nix
@@ -6,6 +6,8 @@
     inputs.nixos-unified.flakeModules.default
     inputs.nixos-unified.flakeModules.autoWire
   ];
+  # https://flake.parts/options/flake-parts#opt-debug
+  # debug = true;
   perSystem = { self', pkgs, ... }: {
     # For 'nix fmt'
     formatter = pkgs.nixpkgs-fmt;
