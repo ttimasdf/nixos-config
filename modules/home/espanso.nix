96b7a904781b0e56a187fe1c200c2bc7a837b44b
ttimasdf <opensource@rabit.pw>
2025-10-30 09:38:29 +0800

    home: espanso: add config

--- modules/home/espanso.nix
+++ modules/home/espanso.nix
@@ -3,4 +3,10 @@
   services.espanso.enable = true;
   # use with rabit.modules.gui.espanso-wayland-fix.enable
   services.espanso.package = pkgs.espanso-wayland;
+
+  services.espanso.configs = {
+    default = {
+      search_shortcut = "ALT+SHIFT+E";
+    };
+  };
 }
