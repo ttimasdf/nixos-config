a3560cd37c756ce9db5ec1611d3dfbcc75ef7f82
ttimasdf <opensource@rabit.pw>
2025-11-04 15:54:33 +0800

    overlays: wps: use latest version from nixpkgs

--- overlays/wps.nix
+++ overlays/wps.nix
@@ -1,25 +1,16 @@
 { flake, lib, ... }:
 
 final: prev:
-let
-  nixpkgs-stable = import flake.inputs.nixpkgs-stable {
-    system = prev.system;
-    config.allowUnfree = true;
-  };
-in
 {
   # Fix wpsoffice-cn with fcitx input method
   # workaround: https://wszqkzqk.github.io/2024/03/09/WPS-Fcitx5/
-  # package version: 12.1.0.17900 from nixos-25.05 https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/wp/wpsoffice-cn/package.nix
-  # installPhase: https://github.com/NixOS/nixpkgs/blob/e643668fd71b949c53f8626614b21ff71a07379d/pkgs/by-name/wp/wpsoffice-cn/package.nix#L108-L127
-  wpsoffice-cn-fixup = nixpkgs-stable.wpsoffice-cn.overrideAttrs (oldAttrs: {
-    installPhase = lib.replaceString "runHook postInstall" ''
+  wpsoffice-cn-fixup = prev.wpsoffice-cn.overrideAttrs (oldAttrs: {
+    preInstallPhases = [ "fixLauncherFcitxPhase" ];
+    fixLauncherFcitxPhase = ''
       for i in $out/share/applications/*; do
         substituteInPlace $i \
           --replace-fail Exec= 'Exec=env GTK_IM_MODULE=fcitx QT_IM_MODULE=fcitx XMODIFIERS=@im=fcitx '
       done
-
-      runHook postInstall
-    '' oldAttrs.installPhase;
+    '';
   });
 }
