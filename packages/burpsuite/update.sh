a36e3b830ebd9f181cddfbe0875add23eaca3898
ttimasdf <opensource@rabit.pw>
2025-11-06 20:56:10 +0800

    pkgs: burpsuite: check update on stable channel

--- packages/burpsuite/update.sh
+++ packages/burpsuite/update.sh
@@ -4,22 +4,24 @@ set -eu -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+json_file="$(mktemp --suffix=.json)"
+
 curl -s 'https://portswigger.net/burp/releases/data' |
   jq -r '
       [[
         .ResultSet.Results[]
         | select(
             (.categories | sort) == (["Professional","Community"] | sort)
-            and .releaseChannels == ["Early Adopter"]
+            and .releaseChannels == ["Stable"]
           )
       ][0].builds[]
       | select(.ProductPlatform == "Jar")
-    ]' >latest.json
+    ]' >"$json_file"
 
-version=$(jq -r '.[0].Version' latest.json)
+version=$(jq -r '.[0].Version' "$json_file")
 
-comm_hex=$(jq -r '.[] | select(.ProductId=="community") .Sha256Checksum' latest.json)
-pro_hex=$(jq -r '.[] | select(.ProductId=="pro") .Sha256Checksum' latest.json)
+comm_hex=$(jq -r '.[] | select(.ProductId=="community") .Sha256Checksum' "$json_file")
+pro_hex=$(jq -r '.[] | select(.ProductId=="pro") .Sha256Checksum' "$json_file")
 
 comm_sri="sha256-$(printf %s "$comm_hex" | xxd -r -p | base64 -w0)"
 pro_sri="sha256-$(printf %s "$pro_hex" | xxd -r -p | base64 -w0)"
@@ -37,3 +39,5 @@ sed -i \
 echo "burpsuite → $version"
 echo "  community: $comm_sri"
 echo "  pro      : $pro_sri"
+
+rm "$json_file"
