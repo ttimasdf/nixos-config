b072172ad424339a22c7c8cef9450c5cde72ce89
ttimasdf <opensource@rabit.pw>
2025-11-06 15:37:36 +0800

    pkgs: burpsuite: add package

---
+++ packages/burpsuite/package.nix
@@ -0,0 +1,136 @@
+{
+  lib,
+  buildFHSEnv,
+  fetchurl,
+  jdk,
+  makeDesktopItem,
+  proEdition ? true,
+  unzip,
+}:
+
+let
+  version = "2025.10.1";
+
+  product =
+    if proEdition then
+      {
+        productName = "pro";
+        productDesktop = "Burp Suite Professional Edition";
+        hash = "sha256-aLP8jVHuKmp4yzcd1KsgidAhWUxoJo0beGwq/6I4n4A=";
+      }
+    else
+      {
+        productName = "community";
+        productDesktop = "Burp Suite Community Edition";
+        hash = "sha256-HiYdJrnTg0HkCt+lXKkhfGawp/NZQmhH4sGytlpiLU8=";
+      };
+
+  src = fetchurl {
+    name = "burpsuite.jar";
+    urls = [
+      "https://portswigger-cdn.net/burp/releases/download?product=${product.productName}&version=${version}&type=Jar"
+      "https://portswigger.net/burp/releases/download?product=${product.productName}&version=${version}&type=Jar"
+      "https://web.archive.org/web/https://portswigger.net/burp/releases/download?product=${product.productName}&version=${version}&type=Jar"
+    ];
+    hash = product.hash;
+  };
+
+  pname = "burpsuite";
+  description = "Integrated platform for performing security testing of web applications";
+  desktopItem = makeDesktopItem {
+    name = "burpsuite";
+    exec = pname;
+    icon = pname;
+    desktopName = product.productDesktop;
+    comment = description;
+    categories = [
+      "Development"
+      "Security"
+      "System"
+    ];
+  };
+
+in
+buildFHSEnv {
+  inherit pname version;
+
+  runScript = ".burplauncher";
+
+  targetPkgs =
+    pkgs: with pkgs; [
+      alsa-lib
+      at-spi2-core
+      cairo
+      cups
+      dbus
+      expat
+      glib
+      gtk3
+      gtk3-x11
+      jython
+      libcanberra-gtk3
+      libdrm
+      udev
+      libxkbcommon
+      libgbm
+      nspr
+      nss
+      pango
+      xorg.libX11
+      xorg.libxcb
+      xorg.libXcomposite
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXrandr
+    ];
+
+  extraInstallCommands = ''
+    mkdir -p "$out/share/pixmaps" "$out/opt/burpsuite"
+    ${lib.getBin unzip}/bin/unzip -p ${src} resources/Media/icon64${product.productName}.png > "$out/share/pixmaps/burpsuite.png"
+    cp -r ${desktopItem}/share/applications $out/share
+    # Symlink both JAR files to the output directory so they're in the same location
+    ln -s ${src} $out/opt/burpsuite/burpsuite_pro_v${version}.jar
+    cp ${./burploader.jar} $out/opt/burpsuite/burploader.jar
+
+    cat <<'EOF' >"$out/bin/.burplauncher"
+    #!/usr/bin/env bash
+    scriptpath="$(dirname "$(readlink -f "$0" || realpath "$0")")"
+    export GDK_SCALE=2
+    exec java -jar "$scriptpath/../opt/burpsuite/burploader.jar"
+    EOF
+
+    cat <<EOF >"$out/opt/burpsuite/.config.ini"
+    auto_run=1
+    early=0
+    ignore=1
+    EOF
+  '';
+
+  passthru.updateScript = ./update.sh;
+
+  meta = with lib; {
+    inherit description;
+    longDescription = ''
+      Burp Suite is an integrated platform for performing security testing of web applications.
+      Its various tools work seamlessly together to support the entire testing process, from
+      initial mapping and analysis of an application's attack surface, through to finding and
+      exploiting security vulnerabilities.
+    '';
+    homepage = "https://portswigger.net/burp/";
+    changelog =
+      "https://portswigger.net/burp/releases/professional-community-"
+      + replaceStrings [ "." ] [ "-" ] version;
+    sourceProvenance = with sourceTypes; [ binaryBytecode ];
+    license = licenses.unfree;
+    platforms = jdk.meta.platforms;
+    hydraPlatforms = [ ];
+    maintainers = with maintainers; [
+      bennofs
+      blackzeshi
+      fab
+      yechielw
+    ];
+    mainProgram = "burpsuite";
+  };
+}
