c54d618a89ca30efe25732430c04a75a42b75c0c
ttimasdf <opensource@rabit.pw>
2025-11-07 16:39:51 +0800

    overlays: wps: makeWrapper

--- overlays/wps.nix
+++ overlays/wps.nix
@@ -5,11 +5,16 @@ final: prev:
   # Fix wpsoffice-cn with fcitx input method
   # workaround: https://wszqkzqk.github.io/2024/03/09/WPS-Fcitx5/
   wpsoffice-cn-fixup = prev.wpsoffice-cn.overrideAttrs (oldAttrs: {
-    preInstallPhases = [ "fixLauncherFcitxPhase" ];
-    fixLauncherFcitxPhase = ''
-      for i in $out/share/applications/*; do
-        substituteInPlace $i \
-          --replace-fail Exec= 'Exec=env GTK_IM_MODULE=fcitx QT_IM_MODULE=fcitx XMODIFIERS=@im=fcitx '
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
+    postFixup = ''
+      # Wrap WPS Office executables with fcitx environment variables
+      for prog in $out/bin/wps $out/bin/wpp $out/bin/et $out/bin/wpspdf; do
+        if [ -f "$prog" ]; then
+          wrapProgram "$prog" \
+            --set GTK_IM_MODULE fcitx \
+            --set QT_IM_MODULE fcitx \
+            --set XMODIFIERS "@im=fcitx"
+        fi
       done
     '';
   });
