08bcffcc82ad68d2f43c1a59ce4d5fda43a9f0f1
ttimasdf <opensource@rabit.pw>
2025-11-10 23:37:42 +0800

    conf: u: refactor into directory

---
+++ configurations/home/u/default.nix
@@ -0,0 +1,26 @@
+{ flake, config, lib, pkgs, isDarwin, ... }:
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports =
+    # Automatically imports everything else in the parent folder.
+    (with builtins;
+    map
+      (fn: ./${fn})
+      (filter (fn: fn != "default.nix") (attrNames (readDir ./.))))
+    ++ [
+      self.homeModules.default
+    ];
+
+  # Defined by /modules/home/me.nix
+  # And used all around in /modules/home/*
+  rabit.me = {
+    username = "u";
+    fullname = "ttimasdf";
+    email = "opensource@rabit.pw";
+  };
+
+  home.stateVersion = "24.11";
+}
---
+++ configurations/home/u/packages.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }:
+{
+  rabit.home.packages.productivity.enable = true;
+  rabit.home.packages.unixtools.enable = true;
+  rabit.home.packages.pentest.enable = true;
+}
