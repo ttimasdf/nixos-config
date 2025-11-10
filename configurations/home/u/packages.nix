930edc6327a24605fcc60aaf842d02ed8b7f92ef
ttimasdf <opensource@rabit.pw>
2025-11-10 23:45:41 +0800

    conf: u: move home packages into user directory

--- configurations/home/u/packages.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-  rabit.home.packages.productivity.enable = true;
-  rabit.home.packages.unixtools.enable = true;
-  rabit.home.packages.pentest.enable = true;
-}
---
+++ configurations/home/u/packages/productivity.nix
@@ -0,0 +1,65 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # File Management & Search
+    fsearch
+    syncthing
+    syncthingtray
+    localsend
+
+    # Note Taking & Documentation
+    siyuan
+    # obsidian
+    typora
+    # pandoc
+
+    # Development & Code Editors
+    # vscode
+
+    # Web Browsers & Download Tools
+    # firefox
+    microsoft-edge
+    aria2
+
+    # Office Suite
+    wpsoffice-cn-fixup
+
+    # Password Management
+    keepassxc
+    git-credential-keepassxc
+
+    # Screenshot & Recording
+    flameshot
+    # asciinema_3
+    obs-studio
+
+    # Media Players
+    vlc
+    qmplay2
+
+    # Networking
+    daed
+    v2rayn
+    tail-tray
+
+    # Remote Access
+    rustdesk-flutter
+    virt-viewer
+    remmina
+  ];
+
+  # Add symlink for edge for Apps to work
+  home.file = {
+    ".local/bin/microsoft-edge-stable".source = "${pkgs.microsoft-edge}/bin/microsoft-edge";
+  };
+
+  programs.firefox.enable = true;
+  programs.obsidian.enable = true;
+  programs.pandoc.enable = true;
+  programs.vscode.enable = true;
+
+  programs.asciinema = {
+    enable = true;
+    package = pkgs.asciinema_3;
+  };
+}
