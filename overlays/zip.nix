50d2d5eb182715dbc2928690248d06fcefd194ea
ttimasdf <opensource@rabit.pw>
2025-11-11 17:34:11 +0800

    overlays: zip: kdePackages.ark use zip-nls

--- overlays/zip.nix
+++ overlays/zip.nix
@@ -1,7 +1,30 @@
 { flake, lib, ... }:
 # Enable NLS (Native Language Support) by adding libnatspec patch from gentoo
 final: prev:
+let
+  inherit (flake.inputs.self) rabit-lib;
+  zip = prev.zip.override { enableNLS = true; };
+  unzip = prev.unzip.override { enableNLS = true; };
+  nlsWrap = pkg:
+    pkg.overrideAttrs (finalAttrs: previousAttrs: {
+      pname = previousAttrs.pname + "-nls";
+      nativeBuildInputs = (previousAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
+      postInstall = ''
+        wrapProgram $out/bin/${previousAttrs.pname} --add-flags "-O gbk"
+      '';
+    });
+  zip-nls = nlsWrap zip;
+  unzip-nls = nlsWrap unzip;
+in
 {
-  zip-nls = prev.zip.override { enableNLS = true; };
-  unzip-nls = prev.unzip.override { enableNLS = true; };
+  zip-nls = zip-nls;
+  unzip-nls = unzip-nls;
+  kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
+    ark = kprev.ark.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ [
+        zip-nls
+        unzip-nls
+      ];
+    });
+  });
 }
