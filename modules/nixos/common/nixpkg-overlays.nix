674158e3cd31a7189499f1f3b090044ce06c5896
ttimasdf <opensource@rabit.pw>
2025-11-12 19:45:59 +0800

    flake: overlays: use rabit-lib.forAllNixFiles instead of manual traversal

--- modules/nixos/common/nixpkg-overlays.nix
+++ modules/nixos/common/nixpkg-overlays.nix
@@ -1,21 +1,11 @@
 { flake, lib, config, pkgs, ... }:
 let
   inherit (flake) self;
-  overlaysPath = self + "/overlays";
-
-  # Recursively find all .nix files in the overlays directory
-  overlayFiles = lib.filesystem.listFilesRecursive overlaysPath;
-
-  # Import each file and apply the necessary arguments
-  importOverlay = path:
-    let
-      imported_module = import path;
-    in
-    # The overlay file is a function that takes flake inputs
-    # and returns the final overlay function.
-      imported_module { inherit flake lib config; };
-
-  overlays = (map importOverlay overlayFiles);
+  inherit (self) rabit-lib;
+  # Load all overlays from the overlays directory
+  overlays =
+    rabit-lib.forAllNixFiles "${self}/overlays"
+      (fn: import fn {inherit flake lib config;});
 
   packagesPath = self + "/packages";
   packages =
@@ -28,5 +18,5 @@ let
 in
 {
   # Map the list of file paths to a list of overlay functions
-  nixpkgs.overlays = overlays ++ [ packages ];
+  nixpkgs.overlays = (builtins.attrValues overlays) ++ [ packages ];
 }
