a3b15da4b670008811b1c100b79a7d65657c616c
ttimasdf <opensource@rabit.pw>
2025-11-12 10:17:25 +0800

    Revert "overlays: wps: use latest version from nixpkgs"
    This reverts commit 18aaff39e4427e155bfb7e43274b85810b0385e0.

--- overlays/wps.nix
+++ overlays/wps.nix
@@ -1,10 +1,18 @@
 { flake, lib, ... }:
 
 final: prev:
+let
+  nixpkgs-stable = import flake.inputs.nixpkgs-stable {
+    system = prev.system;
+    config.allowUnfree = true;
+  };
+in
 {
   # Fix wpsoffice-cn with fcitx input method
   # workaround: https://wszqkzqk.github.io/2024/03/09/WPS-Fcitx5/
-  wpsoffice-cn-fixup = prev.wpsoffice-cn.overrideAttrs (oldAttrs: {
+  # package version: 12.1.0.17900 from nixos-25.05 https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/wp/wpsoffice-cn/package.nix
+  # installPhase: https://github.com/NixOS/nixpkgs/blob/e643668fd71b949c53f8626614b21ff71a07379d/pkgs/by-name/wp/wpsoffice-cn/package.nix#L108-L127
+  wpsoffice-cn-fixup = nixpkgs-stable.wpsoffice-cn.overrideAttrs (oldAttrs: {
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
     postFixup = ''
       # Wrap WPS Office executables with fcitx environment variables
