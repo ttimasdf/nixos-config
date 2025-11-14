b321a629ce2151f79e2cbc553ec16302519cccda
ttimasdf <opensource@rabit.pw>
2025-11-14 21:30:59 +0800

    overlays: ghidra: add GhidraIDA and ghidra_ida_til
    https://github.com/NyaMisty/GhidraIDA
    https://github.com/NyaMisty/ghidra_ida_til

--- overlays/ghidra/default.nix
+++ overlays/ghidra/default.nix
@@ -2,6 +2,18 @@
 
 final: prev:
 let
+  ghidra-ida = prev.fetchFromGitHub {
+    owner = "NyaMisty";
+    repo = "GhidraIDA";
+    rev = "c30ea714e3baaf0b4b1124795e08d283a48d6a92";
+    sha256 = "sha256-pJtY3QeS9rK/L9zVlHr23wfq93aeJXLMvQTkkov94t0=";
+  };
+  ghidra-ida-til = prev.fetchFromGitHub {
+    owner = "NyaMisty";
+    repo = "ghidra_ida_til";
+    rev = "85ac9c9bf1f03e5c82d7f38627eb332a43ee0d68";
+    sha256 = "sha256-FmwEJquJZhb/EJvzxgwVpQd/Nfy/pmI+4wlP0zn6dL4=";
+  };
   patchesDir = ./patches;
   ghidra-patches = lib.pipe patchesDir [
     builtins.readDir
@@ -9,17 +21,29 @@ let
     (lib.filter (name: lib.hasSuffix ".patch" name))
     (lib.map (name: "${patchesDir}/${name}"))
   ];
-  pkg_path = "$out/lib/ghidra";
 in
 {
   ghidra = prev.ghidra.overrideAttrs (oldAttrs: {
     pname = oldAttrs.pname + "-mod";
     patches = (oldAttrs.patches or []) ++ ghidra-patches;
-    # oldAttrs.postFixup:
-    # https://github.com/NixOS/nixpkgs/blob/c5ae371f1a6a7fd27823bc500d9390b38c05fa55/pkgs/tools/security/ghidra/build.nix#L179-L187
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ ghidra-ida ghidra-ida-til ];
+    preBuildPhases = (oldAttrs.preBuildPhases or []) ++ [ "addGhidraIDAPhase" ];
+    preFixupPhases = (oldAttrs.preFixupPhases or []) ++ [ "addIdaTilPhase" "fixHiDPIPhase" ];
+
+    addGhidraIDAPhase = ''
+      defaultToolsDir="./Ghidra/Configurations/Public_Release/src/main/resources/defaultTools"
+      mv $defaultToolsDir/CodeBrowser.tool $defaultToolsDir/CodeBrowserClassic.tool
+      sed -i -e 's@TOOL_NAME="CodeBrowser"@TOOL_NAME="CodeBrowserClassic"@' $defaultToolsDir/CodeBrowserClassic.tool
+      cp ${ghidra-ida}/mistyGhIDA.tool $defaultToolsDir/CodeBrowser.tool
+      # Add new file to certification.manifest
+      echo 'src/main/resources/defaultTools/CodeBrowserClassic.tool||GHIDRA||||END|' >> ./Ghidra/Configurations/Public_Release/certification.manifest
+    '';
+    addIdaTilPhase = ''
+      cp -R ${ghidra-ida-til}/release $out/lib/ghidra/Ghidra/Features/Base/data/typeinfo/ida
+    '';
     # HiDPI Fix:
     # https://gist.github.com/nstarke/baa031e0cab64a608c9bd77d73c50fc6
-    postFixup = oldAttrs.postFixup + ''
+    fixHiDPIPhase = ''
       sed -i 's|VMARGS_LINUX=-Dsun.java2d.uiScale=1|VMARGS_LINUX=-Dsun.java2d.uiScale=2|g' \
         $out/lib/ghidra/support/launch.properties
     '';
