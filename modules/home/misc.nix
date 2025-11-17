dce006e9979b1cd863e5947f4e994b1af7e3f43d
ttimasdf <opensource@rabit.pw>
2025-11-17 23:11:22 +0800

    home: add symlink .local/share/fonts

--- modules/home/misc.nix
+++ modules/home/misc.nix
@@ -6,4 +6,9 @@
     "$HOME/.local/bin"
   ];
 
+  # https://wiki.nixos.org/wiki/Fonts#Flatpak_applications_can't_find_system_fonts
+  # Solution 2, Option 1:
+  # - Symlink to system fonts at $HOME/.local/share/fonts
+  # - Allow access to the fonts folder and /nix/store
+  home.file.".local/share/fonts".source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/X11/fonts";
 }
