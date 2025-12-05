3d5607e9e6dad7c85aa59ffacdf79d05cb5f0aa4
ttimasdf <opensource@rabit.pw>
2025-12-05 16:08:32 +0800

    fix(nixos/viscacha): apply NVIDIA open kernel module patch
    This commit applies a temporary workaround for the NVIDIA open kernel
    modules to resolve an issue with `get_dev_pagemap` changes. The patch is
    fetched from the upstream NVIDIA repository and applied to the stable
    driver package.
    
    Reference: https://github.com/NixOS/nixpkgs/issues/467145

--- configurations/nixos/viscacha/configuration.nix
+++ configurations/nixos/viscacha/configuration.nix
@@ -162,6 +162,23 @@ in
     # allowExternalGpu = true;
   };
 
+  /**
+    FIXME: workaround due to get_dev_pagemap change
+    https://github.com/NixOS/nixpkgs/issues/467145
+   */
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable // {
+    open = config.boot.kernelPackages.nvidiaPackages.stable.open.overrideAttrs (old: {
+      patches = (old.patches or [ ]) ++ [
+        (pkgs.fetchpatch {
+          name = "get_dev_pagemap.patch";
+          url = "https://github.com/NVIDIA/open-gpu-kernel-modules/commit/3e230516034d29e84ca023fe95e284af5cd5a065.patch";
+          hash = "sha256-BhL4mtuY5W+eLofwhHVnZnVf0msDj7XBxskZi8e6/k8=";
+        })
+      ];
+    });
+  };
+
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
