a56eb985f13abe459b33ec9328ad43d99bd75fad
ttimasdf <opensource@rabit.pw>
2025-12-19 09:28:04 +0800

    viscacha: mount crypt-windows

--- configurations/nixos/viscacha/configuration.nix
+++ configurations/nixos/viscacha/configuration.nix
@@ -63,6 +63,11 @@ in
     fsType = "ntfs3";
     options = [ "defaults,rw,nofail,discard,nosuid,uid=1000,dmask=022,fmask=133" ];
   };
+  fileSystems."/mnt/windows" = {
+    device = "/dev/mapper/crypt-windows";
+    fsType = "ntfs3";
+    options = [ "defaults,noauto,rw,nofail,discard,nosuid,uid=1000,dmask=022,fmask=133" ];
+  };
 
   # endregion partitions
 
