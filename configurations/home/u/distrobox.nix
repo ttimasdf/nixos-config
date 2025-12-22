0c0aae54bd5aea80b57037fbda883032ee6c0d7d
ttimasdf <opensource@rabit.pw>
2025-12-22 16:47:56 +0800

    feat(distrobox): add container definitions for debian and alpine
    This commit introduces configuration for Distrobox containers via Home Manager. Specifically, it defines two containers:
    - `debian`: based on Debian 13, including essential build tools, python, and hooks to configure PKU mirrors and alias podman.
    - `alpine`: based on the latest Alpine, including git and build-base.
    
    These additions allow for reproducible and declarative management of development environments within Distrobox.

--- configurations/home/u/distrobox.nix
+++ configurations/home/u/distrobox.nix
@@ -3,6 +3,27 @@
   # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.distrobox.enable
   programs.distrobox = {
     enable = true;
+
+    # https://distrobox.it/usage/distrobox-assemble/
+    containers = {
+      debian = {
+        image = "debian:13";
+        entry = true;
+        additional_packages = "git curl wget build-essential python3-pip nano sudo";
+        pre_init_hooks = [
+          "sed -i -E 's@https?://((deb|security).debian.org|(archive|security).ubuntu.com)@http://mirrors.pku.edu.cn@g' /etc/apt/sources.list.d/*.sources"
+        ];
+        init_hooks = [
+          "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman"
+        ];
+      };
+
+      alpine = {
+        image = "alpine:latest";
+        entry = true;
+        additional_packages="git build-base";
+      };
+    };
   };
 
   home.packages = with pkgs; [
