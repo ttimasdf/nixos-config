46f0e9bbd2c8bbe01d9fec5e26b851be80d7dc6f
ttimasdf <opensource@rabit.pw>
2025-12-22 11:19:32 +0800

    feat(rime-ice): apply patch to fix uuid generation in lua script
    Introduces a patching mechanism to the `fcitx5-rime-ice` overlay using `rabit-lib` to automatically discover patches in the `./patches` directory.
    
    Specifically, this commit adds `fix-uuid-lua.patch` which modifies `lua/uuid.lua` to replace bitwise operators (`&`, `|`) with arithmetic equivalents. This resolves compatibility issues where the Lua environment might not support bitwise operations (e.g., Lua versions older than 5.3).

--- overlays/fcitx5-rime-ice/default.nix
+++ overlays/fcitx5-rime-ice/default.nix
@@ -1,6 +1,9 @@
 { flake, lib, ... }:
 
 final: prev:
+let
+  inherit (flake.inputs.self) rabit-lib;
+in
 {
   # https://github.com/NixOS/nixpkgs/blob/e643668fd71b949c53f8626614b21ff71a07379d/nixos/modules/i18n/input-method/fcitx5.nix#L96-L98
   fcitx5-rime-ice = prev.fcitx5-rime.override {
@@ -9,6 +12,7 @@ final: prev:
 
   # https://github.com/NixOS/nixpkgs/blob/e643668fd71b949c53f8626614b21ff71a07379d/pkgs/by-name/ri/rime-ice/package.nix#L19-L30
   rime-data-ice = prev.rime-ice.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or []) ++ (rabit-lib.findPatches ./patches);
     postInstall = ''
       mv rime_ice_suggestion.yaml default.yaml
     '';
