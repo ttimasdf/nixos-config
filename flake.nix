169b508a0cc9e7362d4957a8fc8a061270d0cbd4
ttimasdf <opensource@rabit.pw>
2026-01-15 15:30:52 +0800

    build(flake): add agenix, agenix-rekey and systems inputs
    Include `agenix` and `agenix-rekey` in flake inputs to establish secret management and secret rekeying capabilities. Additionally, add `systems` and explicitly define `flake-utils` to standardize supported architecture definitions. Update `flake.lock` to capture these new dependencies and their transitive inputs.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   # Principle inputs (updated by `nix run .#update`)
   inputs = {
-    # Helpers
+    # Helpers used as inputs for other flakes
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-unified.url = "github:srid/nixos-unified";
     flake-compat.url = "github:NixOS/flake-compat";
+    systems.url = "github:nix-systems/default";
+    flake-utils = { url = "github:numtide/flake-utils"; inputs.systems.follows = "systems"; };
     git-hooks-nix = { url = "github:cachix/git-hooks.nix"; inputs.nixpkgs.follows = "nixpkgs"; inputs.flake-compat.follows = "flake-compat"; };
 
     # NixOS system flakes
@@ -22,12 +24,30 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.pre-commit.follows = "git-hooks-nix";
     };
-    winapps = { url = "github:winapps-org/winapps"; inputs.nixpkgs.follows = "nixpkgs"; inputs.flake-compat.follows = "flake-compat"; };
+    winapps = {
+      url = "github:winapps-org/winapps";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     nix-index-database = { url = "github:nix-community/nix-index-database"; inputs.nixpkgs.follows = "nixpkgs"; };
     # nixvim = { url = "github:nix-community/nixvim"; inputs.nixpkgs.follows = "nixpkgs"; inputs.flake-parts.follows = "flake-parts"; };
     # vertex = { url = "github:juspay/vertex"; inputs.nixpkgs.follows = "nixpkgs"; };
     nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; inputs.flake-parts.follows = "flake-parts"; };
     nixos-generators = { url = "github:nix-community/nixos-generators"; inputs.nixpkgs.follows = "nixpkgs"; };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+      inputs.darwin.follows = "nix-darwin";
+      inputs.systems.follows = "systems";
+    };
+    agenix-rekey = {
+      url = "github:oddlama/agenix-rekey";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.pre-commit-hooks.follows = "git-hooks-nix";
+    };
   };
 
   # Wired using https://nixos-unified.org/autowiring.html
