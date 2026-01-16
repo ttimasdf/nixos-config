c352780ce45dc073e6f5854960687e4a0f6bb55a
ttimasdf <opensource@rabit.pw>
2026-01-16 08:38:40 +0800

    feat(viscacha): move ZFS and bcachefs to extra-fs specialisation
    Moves the configuration for ZFS (including hostId) and Bcachefs support into a dedicated 'extra-fs' specialisation.
    
    This isolates these filesystems from the main configuration, creating a separate boot entry to load their specific kernel modules and packages only when explicitly selected.

--- configurations/nixos/viscacha/configuration.nix
+++ configurations/nixos/viscacha/configuration.nix
@@ -35,11 +35,13 @@ in
     "net.ipv4.ip_forward" = 1;
   };
 
-  boot.supportedFilesystems.zfs = true;
-  boot.zfs.package = pkgs.zfs_2_4;
-  # ZFS requires networking.hostId to be set
-  networking.hostId = "1858f3ed";
-  boot.supportedFilesystems.bcachefs = true;
+  specialisation.extra-fs.configuration = {
+    boot.supportedFilesystems.zfs = true;
+    boot.zfs.package = pkgs.zfs_2_4;
+    # ZFS requires networking.hostId to be set
+    networking.hostId = "1858f3ed";
+    boot.supportedFilesystems.bcachefs = true;
+  };
 
   # endregion boot & kernel
 
