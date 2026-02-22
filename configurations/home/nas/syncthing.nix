ef983038b611eabdf7f35844ad63e5535366dc4d
ttimasdf <opensource@rabit.pw>
2026-02-22 18:05:32 +0800

    feat: add tmux config symlink for nas and add syncthing source ref
    - Symlink nas tmux.nix to shared u/tmux.nix configuration
    - Add NixOS module source link comment to syncthing config

--- configurations/home/nas/syncthing.nix
+++ configurations/home/nas/syncthing.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, isDarwin, ... }:
 {
+  # https://github1s.com/nix-community/home-manager/blob/master/modules/services/syncthing.nix
   # https://nix-community.github.io/home-manager/options.xhtml#opt-services.syncthing.enable
+  # https://github1s.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/networking/syncthing.nix
   services.syncthing = {
     enable = true;
     guiAddress = "0.0.0.0:8384";
