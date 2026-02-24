95873d6ca331d0380ff165a76151ff8132e42474
ttimasdf <opensource@rabit.pw>
2026-02-24 15:55:51 +0800

    chore: remove example value from no_proxy

--- modules/nixos/common/nixos-options.nix
+++ modules/nixos/common/nixos-options.nix
@@ -15,8 +15,7 @@ in
       };
       no_proxy = lib.mkOption {
         type = lib.types.str;
-        default = "localhost,127.0.0.1,[::1],192.168.0.0/16,172.0.0.0/12,10.0.0.0/8,6.6.0.0/16";
-        example = "localhost,127.0.0.1,[::1],192.168.0.0/16,172.0.0.0/12,10.0.0.0/8";
+        default = "localhost,127.0.0.1,[::1],192.168.0.0/16,172.0.0.0/12,10.0.0.0/8";
         description = "a comma-separated list of domains and IP addresses that should bypass this proxy and be accessed directly";
       };
       myusers = lib.mkOption {
