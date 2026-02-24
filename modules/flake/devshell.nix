ab54e29bb8d1a89d997098105f1a3537e89a9b34
ttimasdf <opensource@rabit.pw>
2026-02-24 17:44:48 +0800

    build: add nixos-eval-config helper to devshell
    Introduce a nixos-eval-config shell function in the devshell module to
    simplify evaluating specific NixOS configuration options for a given
    host directly from the flake.
    
    The function is exported to the environment in the shellHook. Note that
    due to a known direnv issue regarding exported functions, users may
    need to run nix develop manually for the function to be available.
    Additionally, updated .envrc to watch the devshell module for changes.

--- modules/flake/devshell.nix
+++ modules/flake/devshell.nix
@@ -14,6 +14,21 @@
       # Set NH_FLAKE environment variable to the current flake's path
       shellHook = ''
         export NH_FLAKE=$(pwd)
+
+        # FIXME: need to run `nix develop` manually for this function to work.
+        # due to direnv bug:
+        # https://github.com/direnv/direnv/issues/73
+
+        # Function to get nixosConfigurations value from a specific host.
+        function nixos-eval-config() {
+          [ "$#" -lt 2 ] && echo "usage: nixos-eval-config <host> <option> [nix-eval-options]..." && return 1
+          host="$1"
+          shift
+          option="$1"
+          shift
+          nix eval "''${NH_FLAKE}#nixosConfigurations.$host.config.$option" $@
+        }
+        export -f nixos-eval-config
       '';
     };
   };
