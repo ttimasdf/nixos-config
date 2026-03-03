862a24842ccf9ee6ebad76c38830e87dc44ec7a7
ttimasdf <opensource@rabit.pw>
2026-03-03 15:50:08 +0800

    feat(devshell): add gh CLI and guard nixos-eval-config for direnv
    Add GitHub CLI (gh) to devshell packages. Wrap nixos-eval-config
    function in a DIRENV_IN_ENVRC check to skip defining it when running
    under direnv, where it cannot work due to a known direnv limitation
    with exported functions. A warning message is shown instead.

--- modules/flake/devshell.nix
+++ modules/flake/devshell.nix
@@ -8,6 +8,7 @@
         xc
         git-filter-repo
         git-crypt
+        gh
         nixfmt
       ];
 
@@ -18,17 +19,20 @@
         # FIXME: need to run `nix develop` manually for this function to work.
         # due to direnv bug:
         # https://github.com/direnv/direnv/issues/73
-
-        # Function to get nixosConfigurations value from a specific host.
-        function nixos-eval-config() {
-          [ "$#" -lt 2 ] && echo "usage: nixos-eval-config <host> <option> [nix-eval-options]..." && return 1
-          host="$1"
-          shift
-          option="$1"
-          shift
-          nix eval "''${NH_FLAKE}#nixosConfigurations.$host.config.$option" $@
-        }
-        export -f nixos-eval-config
+        if [ "$DIRENV_IN_ENVRC" != "1" ]; then
+          # Function to get nixosConfigurations value from a specific host.
+          function nixos-eval-config() {
+            [ "$#" -lt 2 ] && echo "usage: nixos-eval-config <host> <option> [nix-eval-options]..." && return 1
+            host="$1"
+            shift
+            option="$1"
+            shift
+            nix eval "''${NH_FLAKE}#nixosConfigurations.$host.config.$option" $@
+          }
+          export -f nixos-eval-config
+        else
+          echo "[!] nixos-eval-config not available in direnv, run "nix develop" manually" >&2
+        fi
       '';
     };
   };
