9a5eaa8be3ad92e4353019225aa35f8c6f567a58
toor <toor@example.com>
2026-03-09 16:24:36 +0800

    chore(iot): disable PulseView package in home profile
    Comment out `pulseview` in `configurations/home/toor/packages/iot.nix` to stop installing the signal analyzer by default in the IoT package set.

--- configurations/home/toor/packages/iot.nix
+++ configurations/home/toor/packages/iot.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     openocd       # On-Chip Debugging
-    pulseview     # Signal Analyzer
+    # pulseview     # Signal Analyzer
     # SDR
     uhd           # USRP Hardware Driver
     rkdeveloptool # Rockchip Development Tool
