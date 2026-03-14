31e4e2b68a7dfb1be2421d09cc093225ca2d930d
ttimasdf <opensource@rabit.pw>
2026-03-14 08:55:04 +0800

    feat: update nixpkgs overlays to include llm-agents and currentSystem handling

--- modules/nixos/common/nixpkgs-overlays.nix
+++ modules/nixos/common/nixpkgs-overlays.nix
@@ -2,19 +2,23 @@
 let
   inherit (flake) self;
   inherit (self) rabit-lib;
-  inherit (flake.inputs) private-module;
+  inherit (flake.inputs) private-module llm-agents;
+
+  currentSystem = lib.trace "FIXME: currentSystem pinned to x86_64-linux" "x86_64-linux";
 
   # Manually call packages, as using self.packages here would lead to infinite recursion
   packages =
     final: prev:
       rabit-lib.forAllNixFiles "${self}/packages"
         (fn: lib.callPackageWith final fn { });
-  privatePackages = final: prev: private-module.packages;
+  privatePackages = final: prev: private-module.packages."${currentSystem}";
+  llmagentsPackages = final: prev: llm-agents.packages."${currentSystem}";
 in
 {
   # Map the list of file paths to a list of overlay functions
   nixpkgs.overlays =
     (builtins.attrValues self.overlays)
     ++ (builtins.attrValues private-module.overlays)
-    ++ [ packages privatePackages ];
+    ++ (builtins.attrValues llm-agents.overlays)
+    ++ [ packages privatePackages llmagentsPackages ];
 }
