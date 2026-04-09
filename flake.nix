4fa62a1679c77430e64d9a389b6b21d859164349
ttimasdf <opensource@rabit.pw>
2026-04-09 22:47:49 +0800

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
 
     # NixOS system flakes
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nix-darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
