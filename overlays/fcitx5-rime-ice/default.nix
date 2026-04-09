a16de4f3812198393bb562224d52b549246017ec
ttimasdf <opensource@rabit.pw>
2026-04-09 22:46:53 +0800

    fix(rime-ice): remove external patches from rime-data package
    Remove the application of local patch files from the rime-ice package override. This ensures the build strictly uses the upstream source without applying custom modifications from the `./patches` directory.

--- overlays/fcitx5-rime-ice/default.nix
+++ overlays/fcitx5-rime-ice/default.nix
@@ -12,7 +12,6 @@ in
 
   # https://github.com/NixOS/nixpkgs/blob/e643668fd71b949c53f8626614b21ff71a07379d/pkgs/by-name/ri/rime-ice/package.nix#L19-L30
   rime-data-ice = prev.rime-ice.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or []) ++ (rabit-lib.findPatches ./patches);
     postInstall = ''
       mv rime_ice_suggestion.yaml default.yaml
     '';
