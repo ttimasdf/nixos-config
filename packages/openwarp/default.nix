bac491ae1f960d04fef06d2baf944eb7fedb60fd
ttimasdf <opensource@rabit.pw>
2026-05-09 22:48:31 +0800

    refactor(openwarp): use autoPatchelfHook + runtimeDependencies instead of LD_LIBRARY_PATH wrapper
    Split libraries between buildInputs (compile-time) and runtimeDependencies
    (dlopen'd at runtime) so autoPatchelfHook can set RPATH correctly without
    manually constructing library paths.
    
    - Replace makeWrapper/--prefix LD_LIBRARY_PATH with autoPatchelfHook + runtimeDependencies
    - Use makeBinaryWrapper with WARP_ENABLE_WAYLAND=1 for native Wayland support
    - Add patchelf --add-needed for dlopen'd libs in postFixup
    - Move icon copying from preFixup into installPhase
    - Drop libpulseaudio, mesa, udev from dependency list

--- packages/openwarp/default.nix
+++ packages/openwarp/default.nix
@@ -2,7 +2,7 @@
   fetchurl,
   stdenv,
   autoPatchelfHook,
-  makeWrapper,
+  makeBinaryWrapper,
   lib,
   makeDesktopItem,
   copyDesktopItems,
@@ -15,9 +15,6 @@
   libglvnd,
   libxcb,
   libxkbcommon,
-  libpulseaudio,
-  mesa,
-  udev,
   wayland,
   xz,
   zlib,
@@ -34,36 +31,40 @@ let
     url = "https://github.com/zerx-lab/warp/releases/download/v${version}/warp-terminal-oss_${version}_amd64.deb";
     hash = "sha256-7FSiKewjhS2e41WNzJF8DHA0C6U4Kc+9nWMSyYmn4P0=";
   };
-
-  libraries = [
-    alsa-lib
-    dbus
-    fontconfig
-    libdrm
-    libglvnd
-    libxcb
-    libxkbcommon
-    libpulseaudio
-    mesa
-    udev
-    wayland
-    xz
-    zlib
-    libX11
-    libXcursor
-    libXi
-  ];
 in
   stdenv.mkDerivation (finalAttrs: {
     inherit pname version src;
 
     nativeBuildInputs = [
       autoPatchelfHook
-      makeWrapper
+      makeBinaryWrapper
       copyDesktopItems
       dpkg
     ];
-    buildInputs = libraries;
+
+    buildInputs = [
+      alsa-lib
+      dbus
+      fontconfig
+      (lib.getLib stdenv.cc.cc)
+      xz
+      zlib
+    ];
+
+    runtimeDependencies = [
+      libdrm
+      libglvnd
+      libxcb
+      libxkbcommon
+
+      ## wayland support
+      wayland
+
+      ## X11 support
+      libX11
+      libXcursor
+      libXi
+    ];
 
     unpackPhase = ''
       runHook preUnpack
@@ -79,18 +80,15 @@ in
       # Copy the application files
       cp -r opt/warpdotdev/warp-terminal-oss $out/opt/warpdotdev/
 
-      # Entrypoint wrapper
-      makeWrapper $out/opt/warpdotdev/warp-terminal-oss/warp-oss $out/bin/warp-oss \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath libraries}" \
-        --chdir "$out/opt/warpdotdev/warp-terminal-oss"
-
-      runHook postInstall
-    '';
-
-    # Copy icons from the deb
-    preFixup = ''
+      # Copy icons from the deb
       mkdir -p $out/share/icons
       cp -r usr/share/icons/hicolor $out/share/icons/
+
+      # Wrapper with WARP_ENABLE_WAYLAND=1 for native Wayland support
+      makeWrapper $out/opt/warpdotdev/warp-terminal-oss/warp-oss $out/bin/warp-oss \
+        --set WARP_ENABLE_WAYLAND 1
+
+      runHook postInstall
     '';
 
     desktopItems = [
