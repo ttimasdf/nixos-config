5f63e09c6f848de5db2cdaa624f0c693b6f27417
ttimasdf <opensource@rabit.pw>
2026-06-17 12:06:59 +0800

    chore: remove virt-manager overlay
    the appindicator support was added in https://github.com/NixOS/nixpkgs/pull/499738

--- overlays/virt-manager.nix
+++
@@ -1,8 +0,0 @@
-{ flake, ... }:
-
-final: prev:
-{
-  virt-manager = prev.virt-manager.overrideAttrs (oldAttrs: rec {
-    buildInputs = oldAttrs.buildInputs ++ [ final.libayatana-appindicator ];
-  });
-}
