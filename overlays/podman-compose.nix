bb3f06056c9c2fbf7c3e8b90e7f17666b428579e
ttimasdf <opensource@rabit.pw>
2026-06-22 14:29:03 +0800

    fix(podman-compose): bump from prerelease to upstream 1.6.0
    Pin to the stable release tag instead of an arbitrary prerelease commit.

--- overlays/podman-compose.nix
+++ overlays/podman-compose.nix
@@ -7,12 +7,12 @@ in
 {
   podman-compose = prev.podman-compose.overrideAttrs (oldAttrs: rec {
     # we need --wait from https://github.com/containers/podman-compose/pull/1356
-    version = "1-unstable-2026-04-16";
-    src = lib.trace "FYI: podman-compose pinned to ed3ec99" prev.fetchFromGitHub {
+    version = "1.6.0";
+    src = lib.trace "FYI: podman-compose pinned to ${version}" prev.fetchFromGitHub {
       owner = "containers";
       repo = "podman-compose";
-      rev = "ed3ec99699f692de5440c929ee9e7bfb116871c2";
-      hash = "sha256-OxuWnyxhT6sjH6K5b+7KTx4z8DLLn5sLFUDto9Pa6EY=";
+      rev = "v${version}";
+      hash = "sha256-zkXLZfYWpIaQYoUU7GcGnkuTBmhzpJkyojbzFuTR5FI=";
     };
   });
 }
