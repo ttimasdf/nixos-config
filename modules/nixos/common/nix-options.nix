2d74a030d84b63fe7eea0dc49bcc5ff4e6681d96
ttimasdf <opensource@rabit.pw>
2026-07-11 13:14:10 +0800

    fix(nix): re-enable Tsinghua substituter

--- modules/nixos/common/nix-options.nix
+++ modules/nixos/common/nix-options.nix
@@ -3,10 +3,8 @@
 {
   # Enable Nix Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # FIXME: tsinghua mirror is currently unavailable
-  # Use Chinese nix-channels mirror
-  nix.settings.substituters = lib.trace "FIXME: tsinghua mirror is currently unavailable" [ ];
-  # nix.settings.substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
+  # Use Chinese nix-channels mirror.
+  nix.settings.substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
 
   # Allow unfree packages for the main system configuration (e.g., NVIDIA drivers)
   nixpkgs.config.allowUnfree = true;
