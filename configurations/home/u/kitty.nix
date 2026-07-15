3f6b5690c9b71dfc72b17243c00e0627e7b0815b
ttimasdf <opensource@rabit.pw>
2026-07-15 15:51:51 +0800

    chore(kitty): update keybindings and configuration
    - Explicitly set kitty_mod to ctrl+shift.
    - Remap tab navigation from alt+shift+[0-9] to ctrl+[0-9].
    - Configure new tabs and OS windows to open in the current working directory.
    - Update window detachment shortcuts to use kitty_mod.
    - Add command_palette keybinding to kitty_mod+p.
    - Enable notify_on_cmd_finish to alert when long-running commands complete while the terminal is unfocused.

--- configurations/home/u/kitty.nix
+++ configurations/home/u/kitty.nix
@@ -8,6 +8,7 @@
 
     # kitty.conf - kitty https://sw.kovidgoyal.net/kitty/conf/
     settings = {
+      kitty_mod = "ctrl+shift";  # default, but to be explicit
       scrollback_lines = 99999;
       # background_opacity = 0.85;
       cursor_trail = 1;
@@ -17,25 +18,31 @@
       tab_powerline_style = "round";
       tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{tab.last_focused_progress_percent}{title}";
       tab_title_max_length  = 30;
+
+      notify_on_cmd_finish = "unfocused 10";
     };
 
     # Mappable actions - kitty https://sw.kovidgoyal.net/kitty/actions/
     keybindings = {
       "ctrl+c" = "copy_or_interrupt";  # default to copy_or_noop
+      "kitty_mod+t" = "new_tab_with_cwd";  # default to new_tab
       "ctrl+shift+enter" = "new_window_with_cwd";  # default to new_window
-      "alt+shift+s" = "launch --stdin-source=@screen_scrollback --type=background sh -c 'cat > ~/Documents/kitty-log/$(date +%Y-%m-%d-%H-%M-%S).log'"; # log current terminal buffer
-      "alt+shift+d" = "detach_window new-tab"; # moves the window into a new tab
-      "alt+shift+f" = "detach_window ask"; # asks which tab to move the window into
-      "alt+shift+1" = "goto_tab 1";
-      "alt+shift+2" = "goto_tab 2";
-      "alt+shift+3" = "goto_tab 3";
-      "alt+shift+4" = "goto_tab 4";
-      "alt+shift+5" = "goto_tab 5";
-      "alt+shift+6" = "goto_tab 6";
-      "alt+shift+7" = "goto_tab 7";
-      "alt+shift+8" = "goto_tab 8";
-      "alt+shift+9" = "goto_tab 9";
-      "alt+shift+0" = "goto_tab 10";
+      "kitty_mod+n" = "new_os_window_with_cwd";  # default to new_os_window
+
+      "kitty_mod+p" = "command_palette";
+      # "kitty_mod+s" = "launch --stdin-source=@screen_scrollback --type=background sh -c 'cat > ~/Documents/kitty-log/$(date +%Y-%m-%d-%H-%M-%S).log'"; # log current terminal buffer
+      "kitty_mod+d" = "detach_window new-tab"; # moves the window into a new tab
+      "kitty_mod+f" = "detach_window ask"; # asks which tab to move the window into
+      "ctrl+1" = "goto_tab 1";
+      "ctrl+2" = "goto_tab 2";
+      "ctrl+3" = "goto_tab 3";
+      "ctrl+4" = "goto_tab 4";
+      "ctrl+5" = "goto_tab 5";
+      "ctrl+6" = "goto_tab 6";
+      "ctrl+7" = "goto_tab 7";
+      "ctrl+8" = "goto_tab 8";
+      "ctrl+9" = "goto_tab 9";
+      "ctrl+0" = "goto_tab 10";
     };
 
     # see output of `kitten themes`
