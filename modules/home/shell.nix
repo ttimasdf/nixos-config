0f17b90880e5cd9113aa540a4d7dda377f9ff0e7
ttimasdf <opensource@rabit.pw>
2026-08-16 15:17:59 +0800

    feat(zsh): enable custom completions via fpath
    Add `completionInit` to prepend `$HOME/.zfunc` to `fpath` and run `compinit`, allowing user-defined completions to be loaded automatically.

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -150,6 +150,11 @@ in
         # findNoDups = true;
       };
 
+      completionInit = ''
+       fpath=("$HOME/.zfunc" $fpath)
+       autoload -U compinit && compinit
+     '';
+
       # zsh - What should/shouldn't go in .zshenv, .zshrc, .zlogin, .zprofile, .zlogout? - Unix & Linux Stack Exchange
       # https://unix.stackexchange.com/questions/71253/what-should-shouldnt-go-in-zshenv-zshrc-zlogin-zprofile-zlogout
 
